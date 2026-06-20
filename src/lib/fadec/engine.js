158ad3a4429b18af619f8b775b59ac7031c92a7f
Daniel Nolasco <143855546+nolasco-daniel@users.noreply.github.com>
2026-06-20 16:06:40 +0800

    Update engine.js

--- src/lib/fadec/engine.js
+++ src/lib/fadec/engine.js
@@ -1,6 +1,6 @@
 export class FadecEngine {
   clamp(value, min, max) {
-    return Math.min(max, Math.max(min, value));sssss
+    return Math.min(max, Math.max(min, value));ssssss
   }
 
   compute(state) {
